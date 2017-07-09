250fcaa195e8d3bc66b26d34e87347a3b5450ecf
Tyler Negro <tnegro@gmail.com>
2017-07-08 19:14:03 -0600

    started work on css and js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//game start
+	//choose a random starting number show 19-120
+	//set player score to 0
+	//choose random value for each gem value between 1-12
+
+var wins = 0;
+var losses = 0;
+var goalAmount;
+var score;
+
+function randomNumGen(){
+	
+}
+
+function gameStart(){
+
+}
