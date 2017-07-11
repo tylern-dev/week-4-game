3bd3587811d7aa02f73a0b0e41bb04ea8e133cf9
Tyler Negro <tnegro@gmail.com>
2017-07-10 19:59:33 -0600

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,78 +6,76 @@
 var wins = 0;
 var losses = 0;
 var goalAmount;
-var goalAmountDisplay;
 var score = 0;
-var gem;
 var gemArray = ['diamond','emerald','amethyst', 'pearl'];
 var gemNumber;
 
 
-
-
+//generates the random numbers for the game
 function randomNumGen(min, max){
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random()*(max-min)) + min;	
 }
 
-
+//sends the score to gameLogic() to see if player is winner or not everytime the button is pressed. Also send gemId to gemScore() to get a point value
 function gemButton(){
 	$('button').on('click', function(event){
 		gemId = event.currentTarget.id;
 		gemScore(gemId);
 		gameLogic(score)
 		$('#score-count').html(score)
+
 	});
-		
-	
 }
 
+
+//receive gemId from gemButton() and and check what button was pressed. It then assigns that button a value from the gemNumber array
 function gemScore(buttonId){
 	for(var i=0; i<gemArray.length; i++){
 		if(buttonId===gemArray[i]){
-			console.log('score '+score)
-			console.log('gem Number '+gemNumber)
-			return score += gemNumber[i];
+			score += gemNumber[i];
 		}
 	}
 }
 
-//checks to see if the amount is reached. starts new game
+
+//checks to see if the amount is reached. starts new game.
 function gameLogic(pScore){
 	if(pScore > goalAmount){
 		losses += 1;
 		$('#status-message').html('You lost!');
 		$('#losses-count').html(losses);
-		gameStart();
+		init();
 	}
 	else if(pScore === goalAmount){
 		wins += 1;
 		$('#status-message').html('You Won!');
 		$('#wins-count').html(wins);
-		gameStart();
-	}
+		init();
+	} 
 }
 
+
 //this function starts the game and should reset the score back to 0 and generates a new number 
-function gameStart(){
-	// score=0;
+function init(){
+	score=0;
 	gemNumber = [];
 	goalAmount = randomNumGen(19,120)
-	
 	for(var i = 0; i<4; i++){
 		gemNumber.push(randomNumGen(1,12));
 	}
-	gemButton();
-
 	$('#goal-amount').html(goalAmount);
 	$('#score-count').html(score);
 }
 
 
+function gameStart(){
+	init();
+	gemButton();
+}
 
 gameStart()
-
 
 
 
